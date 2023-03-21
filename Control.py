3358f03a28f3329121fe97e1837090d9b11e6f05
David Antunes <52633777+InfernalGhost@users.noreply.github.com>
2023-03-21 10:21:10 +0000

    Control logic done

--- Control.py
+++ Control.py
@@ -7,15 +7,16 @@ import random
 
 sleep_time = 10
 wait_time = 5
-base_weight = 1000
+base_weight = 500 # Half of the object's weight
 replay = True
 Arduino = serial.Serial('COM_', 57600) # Substitute _ with the USB port being used in the PC (see in ArduinoIDE)
 
 def play_alarm():
-    random_alarm=random.choice(os.listdir("Alarms"))
-    audio = MP3('Alarms/' + random_alarm)
-    playsound('Alarms/' + random_alarm)
+    random_alarm=random.choice(os.listdir("Alarms/Chosen"))
+    audio = MP3('Alarms/Chosen' + random_alarm)
+    playsound('Alarms/Chosen' + random_alarm)
     return audio.info.length
+
 
 while True:
     if (Arduino.inWaiting() > 0):
@@ -23,20 +24,21 @@ while True:
             start = time.time()
             replay = False
         myData = Arduino.readline() 
-        print(myData)
-        print(myData[7:-1])
-        if (myData[0:6] == "value: "): # See if message is about weight values
-            value = eval(myData[7:-1]) # Get value in message
-            if (value > base_weight):
+        myData = myData.decode("utf-8")
+        if (myData[0:6] == "value:"): # See if message is about weight values
+            value = eval(myData[7:-2]) # Get value in message
+            print(value)
+            if (value > - base_weight):
+                print("Playing alarm")
                 replay = True
                 duration = play_alarm()
-                time.sleep(duration + wait_time)
+                time.sleep(sleep_time)
+                Arduino.reset_input_buffer()
+                print("Checking")
             else:
                 now = time.time()
                 if (now - start >= wait_time):
+                    print("Finishing")
                     break
-            
-
-
 
 
--- Read_Value/Read_Value.ino
+++ Read_Value/Read_Value.ino
@@ -41,13 +41,12 @@ void setup() {
   LoadCell.begin();
   //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
   float calibrationValue; // calibration value (see example file "Calibration.ino")
-  calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
+  //calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
 #if defined(ESP8266)|| defined(ESP32)
   //EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
 #endif
   EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
-
-  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  unsigned long stabilizingtime = 5000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
   boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
   LoadCell.start(stabilizingtime, _tare);
   if (LoadCell.getTareTimeoutFlag()) {
@@ -62,7 +61,7 @@ void setup() {
 
 void loop() {
   static boolean newDataReady = 0;
-  const int serialPrintInterval = 250; //increase value to slow down serial print activity
+  const int serialPrintInterval = 100; //increase value to slow down serial print activity
 
   // check for new data/start next conversion:
   if (LoadCell.update()) newDataReady = true;
